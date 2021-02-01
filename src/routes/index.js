556ff65a6461dd4cbb07dba3fef6e73aacc98507
Gerd Watmann <watmann@de.ibm.com>
2021-02-01 23:22:35 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo Feb 2nd 2022' });
+  res.render('index', { title: 'Cloud Native demo Feb 3nd 2022' });
 });
 
 module.exports = router;
