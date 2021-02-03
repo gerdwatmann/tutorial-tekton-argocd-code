88f681c8f0d89c4e345cfd76d8fe8224498760f2
Gerd Watmann <pair+gerd@garage>
2021-02-03 14:24:11 +0100

    demo commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Cloud Native demo Feb 5nd 2021" });
+  res.render("index", { title: "Cloud Native demo Feb 10nd 2021" });
 });
 
 module.exports = router;
